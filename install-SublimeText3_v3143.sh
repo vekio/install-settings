f3798df7fcf3feb182d5cb28eecb5c713ce2bf80
vekio <casta94@outlook.com>
2017-10-29 23:12:25 +0100

    Add SublimeText3 Install script

---
+++ install-SublimeText3_v3143.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+################################################################################
+# Written to be used on 64 bits computers
+# Author    :   vekio
+# Github    :   https://github.com/vekio
+################################################################################
+################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+################################################################################
+
+rm /tmp/sublime-text_build-3143_amd64.deb
+
+wget https://download.sublimetext.com/sublime-text_build-3143_amd64.deb -O /tmp/sublime-text_build-3143_amd64.deb
+sudo dpkg -i /tmp/sublime-text_build-3143_amd64.deb
+
+rm /tmp/sublime-text_build-3143_amd64.deb
+
+# ./install-SublimeText3_vekioSettings.sh
+
+echo "################################################################"
+echo "##############     Sublime Text 3 installed      ###############"
+echo "################################################################"
