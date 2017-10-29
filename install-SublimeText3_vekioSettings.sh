67faa2e710899d44049f70d4841a9a284de35896
vekio <casta94@outlook.com>
2017-10-29 23:42:05 +0100

    Add ST3 Settings

---
+++ install-SublimeText3_vekioSettings.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+################################################################################
+# Written to be used on 64 bits computers
+# Author    :   vekio
+# Github    :   https://github.com/vekio
+################################################################################
+################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+################################################################################
+
+# Package Control Install - https://packagecontrol.io/installation
+# wget https://packagecontrol.io/Package%20Control.sublime-package -P $HOME/.config/sublime-text-3/Installed Packages/
+
+# ColorSublime - https://packagecontrol.io/packages/Colorsublime
+cd $HOME/.config/sublime-text-3/Packages/
+git clone https://github.com/Colorsublime/Colorsublime-Plugin.git
+
+echo "################################################################"
+echo "##############     vekio settings installed      ###############"
+echo "################################################################"
